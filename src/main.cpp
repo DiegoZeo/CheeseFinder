22b33a3f7f867576696e4cf19ffd22ce2c7652af
dazeo <da.zeolla2004@gmail.com>
2024-05-03 20:06:05 +0200

    Added Map logic (20x20 matrix) and render (map_grid drawing on screen)

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,90 @@
 #include <SFML/Graphics.hpp>
 
+// The world map
+
+const int MAP_WIDTH = 20;
+const int MAP_HEIGHT = 20;
+
+int world_map[ MAP_WIDTH * MAP_HEIGHT ] =
+        {
+
+// 0001020304050607080910111213141516171819
+                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,   // 00
+                1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,1,   // 01
+                1,9,9,1,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 02
+                1,9,9,1,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 03
+                1,9,1,1,1,1,9,9,1,9,1,9,1,1,1,1,9,9,1,1,   // 04
+                1,9,1,1,9,1,1,1,1,9,1,1,1,1,9,1,1,1,1,1,   // 05
+                1,9,9,9,9,1,1,1,1,1,1,9,9,9,9,1,1,1,1,1,   // 06
+                1,9,9,9,9,9,9,9,9,1,1,1,9,9,9,9,9,9,9,1,   // 07
+                1,9,1,1,1,1,1,1,1,1,1,9,1,1,1,1,1,1,1,1,   // 08
+                1,9,1,9,9,9,9,9,9,9,1,1,9,9,9,9,9,9,9,1,   // 09
+                1,9,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,1,1,   // 10
+                1,9,9,9,9,9,1,9,1,9,1,9,9,9,9,9,1,1,1,1,   // 11
+                1,9,1,9,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 12
+                1,9,1,9,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 13
+                1,9,1,1,1,1,9,9,1,9,1,9,1,1,1,1,9,9,1,1,   // 14
+                1,9,1,1,9,1,1,1,1,9,1,1,1,1,9,1,1,1,1,1,   // 15
+                1,9,9,9,9,1,1,1,1,1,1,9,9,9,9,1,1,1,1,1,   // 16
+                1,1,9,9,9,9,9,9,9,1,1,1,9,9,9,1,9,9,9,9,   // 17
+                1,9,1,1,1,1,1,1,1,1,1,9,1,1,1,1,1,1,1,1,   // 18
+                1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,   // 19
+
+        };
+
+int GetMap( int x, int y )
+{
+    if( x < 0 ||
+        x >= MAP_WIDTH ||
+        y < 0 ||
+        y >= MAP_HEIGHT
+            )
+    {
+        return 9;
+    }
+
+    return world_map[(y*MAP_WIDTH)+x];
+}
+
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    sf::Uint32 style = sf::Style::Titlebar | sf::Style::Close;
+    auto window = sf::RenderWindow{ { 800u, 800u }, "ChesseFinder", style};
     window.setFramerateLimit(144);
+
+    sf::RectangleShape cell(sf::Vector2f (40,40));
+
 
     while (window.isOpen())
     {
         for (auto event = sf::Event{}; window.pollEvent(event);)
         {
-            if (event.type == sf::Event::Closed)
+            switch(event.type)
             {
-                window.close();
+                case sf::Event::EventType::Closed:
+                    window.close();
+                    break;
+                case sf::Event::EventType::KeyPressed:
+                    if(event.key.code == sf::Keyboard::Key::Escape)
+                        window.close();
+                        break;
             }
         }
 
         window.clear();
+
+        for(int i = 0; i<MAP_HEIGHT; i++)
+        {
+            for(int j = 0; j<MAP_HEIGHT; j++)
+            {
+                if (GetMap(j,i) == 1)
+                    cell.setFillColor(sf::Color::White);
+                else
+                    cell.setFillColor(sf::Color::Black);
+                cell.setPosition(j * MAP_HEIGHT * 2, i * MAP_WIDTH * 2);
+                window.draw(cell);
+            }
+        }
         window.display();
     }
 }
